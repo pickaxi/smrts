a655ca2bd96d61c92271c733f329fe72ac5994e6
K R $ H N A <patrioremrej@gmail.com>
2025-10-19 07:31:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,13 @@
-
-
 from os import getenv
-
-# VPS --- FILL COOKIES 🍪 in """ ... """ 
-
 INST_COOKIES = """
-# wtite up here insta cookies
 """
-
 YTUB_COOKIES = """
-# write here yt cookies
 """
-
-API_ID = int(getenv("API_ID", "28735699"))
-API_HASH = getenv("API_HASH", "2e19c326d8cb322df7c15d7b7e84d1f3")
-BOT_TOKEN = getenv("BOT_TOKEN", "8223449688:AAHmFsXJAeyGC0VHLHoRX5EOzxCj0toTr3Q")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7641937093 6573328336").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ridhatlog:RcZ4EVi6hzwSaarZ@cluster0.dxj0ntg.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
 #LOG_GROUP = getenv("LOG_GROUP", "-1002879768901")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002504099057"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "500"))
@@ -25,5 +16,5 @@ WEBSITE_URL = getenv("WEBSITE_URL", "")
 AD_API = getenv("AD_API", "")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
+DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  
 INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
